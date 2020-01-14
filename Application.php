e2b4056b4b1a230c38934144619fd076bc1016a7
Dennis Fredsson <dennis.fredsson@hotmail.com>
2020-01-14 19:15:35 +0100

    rewrite as console app

--- Application.php
+++ Application.php
@@ -2,13 +2,33 @@
 
 class Application
 {
-
-    public function __construct()
-    {
-    }
-
     public function run()
     {
-        echo 'hello world';
+        $programIsRunning = true;
+
+        while ($programIsRunning) {
+
+            $this->printMenu();
+
+            $choice = trim(fgets(STDIN));
+
+            switch ($choice) {
+                case '0':
+                    $programIsRunning = false;
+                    break;
+            }
+        }
+    }
+
+    function printMenu()
+    {
+
+        echo "************ Garden Tracker ******************\n";
+        echo "1 - Show all data\n";
+        echo "2 - Get coldest hour\n";
+        echo "3 - \n";
+        echo "4 - \n";
+        echo "0 - Quit\n";
+        echo "Enter your choice: ";
     }
 }
